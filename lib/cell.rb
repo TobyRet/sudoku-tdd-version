d06636164223b926a13467fd9735d9af158d3a70
Toby Retallick <toby.retallick@gmail.com>
2014-03-13 11:20:17 +0000

    Cell knows if it has a unsolved value (0)

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,17 @@
 class Cell
 
   attr_accessor :value
-  
+
   def initalize 
     @value = nil
+  end
+
+  def unsolved
+    @value = 0
+  end
+
+  def filled_out?
+    @value != 0
   end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -10,6 +10,19 @@ describe 'Cell' do
       expect(cell.value).to eq(nil)
     end
 
+    it "can have a status of unsolved" do
+      expect(cell.unsolved).to eq(0)
+    end
+
+    it "can have a status of solved" do
+      expect(cell.unsolved).to eq(0)
+    end
+
+    it "should know if it has an unsolved value" do
+      cell.unsolved
+      expect(cell.filled_out?).to eq(false)
+    end
+
   end
 
 end
